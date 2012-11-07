1bb7740fd66156a17e60dea112851e0fe949c230
Bronson Zgeb <bronson.zgeb@gmail.com>
2012-11-07 17:24:42 -0500

    Final-ish build

--- Assets/Scripts/MemoryWindow.cs
+++ Assets/Scripts/MemoryWindow.cs
@@ -4,15 +4,21 @@ using System.Collections;
 public class MemoryWindow : MonoBehaviour {
 
 	private bool has_played;
+	private Texture2D old_mood;
+	public Texture2D silk_mood;
+	private Inventory inventory;
+	
 	void Start()
 	{
 		has_played = false;
+		inventory = FindObjectOfType(typeof(Inventory)) as Inventory;
 	}
 	
 	void OnClick()
 	{
 		if (!has_played)
 		{
+			inventory.mood_tex = silk_mood;
 			MovieControl ctl = FindObjectOfType(typeof(MovieControl)) as MovieControl;
 			ctl.PlayMovie(1);
 			has_played = true;
